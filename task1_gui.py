66bdd9623c9029ae7b1741ea87f65cdb5e134b7d
Denys Murakhovskyi <dmurakhowsky@gmail.com>
2021-03-03 00:39:52 +0200

    changed vision

--- task1_gui.py
+++ task1_gui.py
@@ -13,29 +13,34 @@ class App:
         # создание основного окна Tkinter
         self.window = Tk()
         self.window.title("Треугольники")
-        self.window.geometry('500x460')
-        self.window.minsize(500, 480)
-        self.window.maxsize(500, 480)
+        self.window.geometry('520x520')
+        self.window.minsize(520, 520)
+        self.window.maxsize(520, 520)
 
         self.press_x, self.press_y = 0, 0
         self.release_x, self.release_y = 0, 0
 
+        # флаги нажатия/отжатия кнопки
+        self.pressed = False
+
         # размещение элементов интерфейса
 
         # основной элемент - Canvas
-        self.c = Canvas(self.window, width=450, height=250, bg='black')
-        self.c.pack(side=TOP, padx=10, pady=(10, 0))
+        self.canvas_x = 470
+        self.canvas_y = 270
+        self.c = Canvas(self.window, width=self.canvas_x, height=self.canvas_y, bg='black')
+        self.c.pack(side=TOP, padx=10, pady=(10, 10))
 
         # элементы управления: настройки, отображение параметров, очистка/выход
-        setup = Frame(self.window, width=450)
-        setup_left = Frame(setup, width=250)
-        setup_right = Frame(setup, width=180)
+        setup = Frame(self.window, width=470)
+        setup_left = Frame(setup, width=270)
+        setup_right = Frame(setup, width=200)
 
         self.setup_notebook = Notebook(setup_left)
 
-        setup1 = Frame(setup_left, width=230, height=140)
-        setup2 = Frame(setup_left, width=230, height=140)
-        setup3 = Frame(setup_left, width=230, height=140)
+        setup1 = Frame(setup_left, width=250, height=140)
+        setup2 = Frame(setup_left, width=250, height=140)
+        setup3 = Frame(setup_left, width=250, height=140)
 
         # элементы управления на вкладках
         # вкладка 1
@@ -89,35 +94,58 @@ class App:
         self.params = Treeview(setup_right, show='headings', columns=columns, height=5)
         self.params.heading('#1', text='Параметр')
         self.params.heading('#2', text='Значение')
-        self.params.column('#1', width=100, minwidth=50, stretch=NO, anchor=N)
-        self.params.column('#2', width=100, minwidth=50, stretch=NO, anchor=N)
-        self.params.pack(side=TOP, padx=(15, 0), pady=(27, 5))
+        self.params.column('#1', width=110, minwidth=50, stretch=NO, anchor=N)
+        self.params.column('#2', width=110, minwidth=50, stretch=NO, anchor=N)
+        self.params.pack(side=TOP, padx=(15, 0), pady=(15, 5))
 
         butframe = Frame(setup_right)
         Button(butframe, text='Очистить', width=10, command=self.draw_base()).pack(side=LEFT, padx=(25, 5))
         Button(butframe, text='Выход', width=10, command=lambda x=0: sys.exit(x)).pack(side=LEFT, padx=(0, 10))
-        butframe.pack(side=TOP, pady=(5, 5))
+        butframe.pack(side=TOP, pady=(17, 5))
 
-        setup_left.pack(side=LEFT)
+        setup_left.pack(side=LEFT, padx=(0, 20))
         setup_right.pack(side=LEFT)
         setup.pack(side=TOP, pady=(5, 10), padx=(5, 5))
 
         self.window.bind('<Button-1>', self.press)
         self.window.bind('<ButtonRelease-1>', self.release)
+        self.window.bind('<Motion>', self.motion)
 
         self.draw_base()
         self.window.mainloop()
 
+    def motion(self, event):
+        if self.pressed:
+            if event.widget.master is not None:
+                if event.widget.widgetName == 'canvas':
+                    self.draw_base()
+                    self.c.create_line([self.press_x, self.press_y, event.x, event.y], dash=True, fill='yellow')
+
     def draw_base(self):
         self.c.delete("all")
-        self.c.create_line([5, 125, 445, 125], arrow=LAST, fill='white')
-        self.c.create_line([225, 245, 225, 5], arrow=LAST, fill='white')
-        self.c.create_text(442, 135, text='x', fill='white')
-        self.c.create_text(235, 8, text='y', fill='white')
+        # базовые оси
+        self.c.create_line([10, 250, 460, 250], arrow=LAST, fill='white')
+        self.c.create_line([20, 260, 20, 10], arrow=LAST, fill='white')
+
+        # метки
+        for i in range(1, 5):
+            self.c.create_line([100*i + 20, 247, 100*i + 20, 253], fill='white')
+        for i in range(1, 3):
+            self.c.create_line([17, 250 - 100*i, 23, 250 - 100*i], fill='white')
+
+        # надписи
+        # наименование осей
+        self.c.create_text(457, 258, text='x', fill='white')
+        self.c.create_text(30, 15, text='y', fill='white')
+        # наименование меток
+        for i in range(0, 5):
+            self.c.create_text(100*i + 25, 260, text=str(100*i), fill='white')
+        for i in range(1, 3):
+            self.c.create_text(34, 250 - 100*i, text=str(100 * i), fill='white')
 
     def press(self, event):
         """
-        Обработчкик события "нажатие кнопки мыши"
+        Обработчик события "нажатие кнопки мыши"
         :param event:
         :return:
         """
@@ -125,6 +153,7 @@ class App:
             if event.widget.widgetName == 'canvas':
                 self.draw_base()
                 self.press_x, self.press_y = event.x, event.y
+                self.pressed = True
 
     def release(self, event):
         if event.widget.master is not None:
@@ -132,11 +161,19 @@ class App:
                 self.release_x, self.release_y = event.x, event.y
                 if (self.release_x in range(450)) & (self.release_y in range(250)):
                     self.draw_triangle(None)
+        self.pressed = False
+
+    def check_coordinates(self, C):
+        if (C[0] < 0) | (C[1] < 0):
+            return False
+        if (C[0] > self.canvas_x) | (C[1] > self.canvas_y):
+            return False
+        return True
 
     def draw_triangle(self, event):
         if (self.press_x > 0) & (self.press_y > 0) & (self.release_x > 0) & (self.release_y > 0):
             self.draw_base()
-            triangle = Math((self.press_x, self.press_y), (self.release_x, self.release_y))
+            triangle = Math((self.press_x, self.press_y), (self.release_x, self.release_y), (20, 250))
             task = self.setup_notebook.index(self.setup_notebook.select()) + 1
             if task == 1:
                 data_dict = {'b': self.scal_b.get(), 'alpha': self.scal_alpha.get()}
@@ -147,14 +184,14 @@ class App:
             else:
                 return
             C1, C2, data_dict = triangle.get_c(task, data_dict)
-            if (C1[0] < 0) | (C1[1] < 0) | (C2[0] < 0) | (C2[1] < 0):
-                self.c.create_text(300, 100, text='Невозможно выполнить построение', fill='white')
-            else:
+            if self.check_coordinates(C1) & self.check_coordinates(C2):
                 self.c.create_polygon([self.press_x, self.press_y, self.release_x, self.release_y, C1[0], C1[1]],
                                       fill='red')
                 self.c.create_polygon([self.press_x, self.press_y, self.release_x, self.release_y, C2[0], C2[1]],
                                       fill='blue')
                 self.update_treeview(data_dict)
+            else:
+                self.c.create_text(300, 100, text='Одна из точек вне области построения', fill='white')
 
     def update_treeview(self, data):
         """
@@ -168,13 +205,30 @@ class App:
 
 
 class Math:
-    def __init__(self, A, B):
-        self.A, self.B = A, B
+    def __init__(self, A, B, zero):
+        self.A, self.B = self.shift_x(A), self.shift_y(B)
         self.C1, self.C2 = (0, 0), (0, 0)
         Ax, Ay = self.A[0], self.A[1]
         Bx, By = self.B[0], self.B[1]
         self.phi = atan((By-Ay) / (Bx-Ax)) * 180 / 3.1415
         self.c = sqrt((Ax-Bx)**2 + (Ay-By)**2)
+        self.zero_x = zero[0]
+        self.zero_y = zero[1]
+
+    def shift_x(self, x):
+        return x - self.zero_x
+
+    def shift_y(self, y):
+        return self.zero_y - y
+
+    def ashift_x(self, x):
+        return x + self.zero_x
+
+    def ashift_y(self, y):
+        return self.zero_y - y
+
+    def acoords(self, C):
+        return tuple(self.ashift_x(C[0]), self.ashift_y(C[1]))
 
     def get_c(self, case, data):
         if case == 1:
@@ -183,11 +237,15 @@ class Math:
             self.C1, self.C2 = self.case2(self.c, self.phi, self.A, data['alpha'], data['beta'])
         elif case == 3:
             self.C1, self.C2 = self.case3(self.c, self.phi, self.A, data['a'], data['b'])
-        return self.C1, self.C2, {'A': '({:d}, {:d})'.format(int(self.A[0]), int(self.A[1])),
-                                  'B': '({:d}, {:d})'.format(int(self.B[0]), int(self.B[1])),
+        return self.C1, self.C2, {'A': '({:d}, {:d})'.format(int(self.A[0]),
+                                                             int(self.A[1])),
+                                  'B': '({:d}, {:d})'.format(int(self.B[0]),
+                                                             int(self.B[1])),
                                   'phi': int(self.phi),
-                                  'C1': '({:d}, {:d})'.format(int(self.C1[0]), int(self.C1[1])),
-                                  'C2': '({:d}, {:d})'.format(int(self.C2[0]), int(self.C2[1]))}
+                                  'C1': '({:d}, {:d})'.format(int(self.C1[0]),
+                                                              int(self.C1[1])),
+                                  'C2': '({:d}, {:d})'.format(int(self.C2[0]),
+                                                              int(self.C2[1]))}
 
     def calcB(self, c, phi, A):
         """
