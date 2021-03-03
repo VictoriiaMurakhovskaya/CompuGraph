a13ecafbd117f64adf559a4cc68fb31249f50e31
Denys Murakhovskyi <dmurakhowsky@gmail.com>
2021-03-03 22:56:20 +0200

    Rotating mirror

--- task3.py
+++ task3.py
@@ -1,38 +1,9 @@
-from tkinter import Tk, Frame, LabelFrame, Canvas, TOP, LEFT, Scale, HORIZONTAL, Button, NO, LAST, END, N
+from tkinter import Tk, Frame, LabelFrame, Canvas, TOP, LEFT, Scale, HORIZONTAL
+from tkinter import Button, NO, LAST, END, N, Label, W
 from tkinter.ttk import Treeview
 import numpy as np
 import sys
 from math import sqrt
-
-press_x, press_y = 0, 0
-temp_x, temp_y = 0, 0
-x_fin, y_fin = 0, 0
-x_cross, y_cross = 0, 0
-mirror_border = 0
-arrow_state = False
-a = 0.002
-params = None
-
-
-def draw_parabola(canv):
-    """
-    расчет и отрисовка параболического зеркала
-    :param canv:
-    :return:
-    """
-    global a, mirror_border
-    xc, yc = 30, 115
-    y1 = np.ones(200)
-    y1 = y1.cumsum() - 100 + yc
-    x1 = a * (y1 - yc) ** 2 + xc
-    mirror_border = max(x1)
-
-    c1 = np.empty(len(y1) * 2)
-    c1[0::2], c1[1::2] = x1, y1
-    c1 = c1.astype(int).copy()
-    canv.create_line(list(c1), width=3, fill='red')
-
-    canv.create_oval(30, 115, 25, 120, fill='green')
 
 
 def draw_beam(canv):
@@ -115,57 +86,6 @@ def draw_mirrored(c, x0, y0):
     c.create_line([x0, y0, x1, y1], width=2, fill='magenta')
 
 
-def main():
-    """
-    основной метод - создание графического интерфейса TKinter
-    :return:
-    """
-    global params
-    window = Tk()
-    window.title("Отражение в параболическом зеркале")
-    window.geometry('500x400')
-    canv = Frame(window)
-    setup = LabelFrame(window, text='Настройки', width=450, height=150)
-    c = Canvas(canv, width=450, height=230, bg='black')
-    c.pack(side=TOP, padx=10)
-
-    setup1 = Frame(setup)
-    butFrame = Frame(setup1)
-
-    scal = Scale(setup1, orient=HORIZONTAL, length=200, from_=0.002, to=0.01, tickinterval=0.002, resolution=0.002)
-    scal.set(0.002)
-    scal.pack(side=TOP, pady=(0, 10), padx=(10, 10))
-    scal.bind("<ButtonRelease-1>", lambda event, draw_canv=c: redraw(event, draw_canv))
-
-    Button(butFrame, text='Очистить', width=12,
-           command=lambda event=None, draw_canv=c, flag=True: redraw(event, draw_canv,
-                                                                     setarrow=flag)) \
-        .pack(side=LEFT, padx=(10, 5), pady=(5, 10))
-    Button(butFrame, text='Закрыть', width=12, command=lambda flag=0: sys.exit(flag)).pack(side=LEFT, padx=(5, 10),
-                                                                                           pady=(5, 10))
-
-    butFrame.pack(side=TOP)
-
-    setup1.pack(side=LEFT)
-
-    columns = ('#1', '#2')
-    params = Treeview(setup, show='headings', columns=columns)
-    params.heading('#1', text='Параметр')
-    params.heading('#2', text='Значение')
-    params.column('#1', width=100, minwidth=50, stretch=NO, anchor=N)
-    params.column('#2', width=100, minwidth=50, stretch=NO, anchor=N)
-    params.pack(side=LEFT, padx=(5, 10), pady=(0, 10))
-
-    canv.pack(side=TOP, pady=(10, 10))
-    setup.pack(side=TOP, pady=(0, 10), padx=(25, 25))
-
-    draw_parabola(c)
-
-    window.bind('<Button-1>', press)
-    window.bind('<ButtonRelease-1>', lambda event, draw_canv=c: release(event, draw_canv))
-    window.mainloop()
-
-
 def redraw(event, c, setarrow=False):
     """
     перерисовка изображения на canvas
@@ -186,7 +106,6 @@ def redraw(event, c, setarrow=False):
     if arrow_state:
         c.create_line([press_x, press_y, x_fin, y_fin], width=2, fill='blue', arrow=LAST)
         draw_beam(c)
-    draw_parabola(c)
     update_treeview()
 
 
@@ -216,7 +135,6 @@ def release(event, c):
                 x_fin, y_fin = event.x, event.y
                 press_x, press_y = temp_x, temp_y
                 c.delete('all')
-                draw_parabola(c)
                 arrow_state = True
                 c.create_line([press_x, press_y, x_fin, y_fin], width=2, fill='blue', arrow=LAST)
                 draw_beam(c)
@@ -233,6 +151,126 @@ def update_treeview():
         params.insert("", END, values=['(x1, y1)', '(' + str(press_x) + ', ' + str(press_y) + ')'])
         params.insert("", END, values=['(x2, y2)', '(' + str(x_fin) + ', ' + str(y_fin) + ')'])
         params.insert("", END, values=['(x_cross, y_cross)', '(' + str(int(x_cross)) + ', ' + str(int(y_cross)) + ')'])
+
+
+class Math:
+
+    def __init__(self, xc, yc):
+        self.mirror_x = xc
+        self.mirror_y = yc
+
+    def mirror(self, a, phi):
+        """
+        Расчет координат параболы, изображающей зеркало
+        :param a: кривизна параболы (к-т уравнения при x^2)
+        :param phi: угол наклона параболы
+        :return: список координат параболы для отображения, правую x-координату параболического зеркала
+        """
+        xc, yc = self.mirror_x, self.mirror_y
+        phi = phi / 180 * 3.1415
+        y1 = np.ones(180)
+        y1 = y1.cumsum() - 90 + yc
+        x1 = a * (y1 - yc) ** 2 + xc
+
+        x2 = (x1 - self.mirror_x) * np.cos(phi) + (y1 - self.mirror_y) * np.sin(phi) + self.mirror_x
+        y2 = -1 * (x1 - self.mirror_x) * np.sin(phi) + (y1 - self.mirror_y) * np.cos(phi) + self.mirror_y
+
+        mirror_border = max(x2)
+
+        c1 = np.empty(len(y2) * 2)
+        c1[0::2], c1[1::2] = x2, y2
+        c1 = c1.astype(int).copy()
+
+        return c1, mirror_border
+
+
+class App:
+
+    def __init__(self, canvas_y):
+        # инициализация переменных
+        self.mirror_border = 0
+        self.press_x, self.press_y = 0, 0
+        self.temp_x, self.temp_y = 0, 0
+        self.x_fin, self.y_fin = 0, 0
+        self.x_cross, self.y_cross = 0, 0
+        self.canvas_height = canvas_y
+        self.x_center, self.y_center = 100, int(self.canvas_height / 2)
+
+        self.math = Math(self.x_center, self.y_center)
+        self.mirror_border = 0
+
+        # arrow_state = False
+        # a = 0.002
+        # params = None
+
+        window = Tk()
+        window.title("Отражение в параболическом зеркале")
+        window.geometry('600x'+str(self.canvas_height + 250))
+        window.minsize(600, self.canvas_height + 250)
+        window.maxsize(600, self.canvas_height + 250)
+        canv = Frame(window)
+        setup = LabelFrame(window, text='   Настройки   ', width=450, height=150)
+        self.c = Canvas(canv, width=550, height=self.canvas_height, bg='black')
+        self.c.pack(side=TOP, padx=10)
+
+        setup1 = Frame(setup)
+        setup2 = Frame(setup)
+        butFrame = Frame(setup2)
+
+        Label(setup1, text='Форма зеркала').pack(side=TOP, padx=(10, 0), anchor=W)
+        self.scal = Scale(setup1, orient=HORIZONTAL, length=300, from_=0, to=0.015, tickinterval=0.005, resolution=0.001)
+        self.scal.set(0.002)
+        self.scal.pack(side=TOP, pady=(0, 10), padx=(10, 10))
+        self.scal.bind("<ButtonRelease-1>", self.redraw)
+
+        Label(setup1, text='Наклон зеркала').pack(side=TOP, padx=(10, 0), anchor=W)
+        self.angle = Scale(setup1, orient=HORIZONTAL, length=300, from_=-45, to=45, tickinterval=15, resolution=15)
+        self.angle.set(0)
+        self.angle.pack(side=TOP, pady=(0, 10), padx=(10, 10))
+        self.angle.bind("<ButtonRelease-1>", self.redraw)
+
+        Button(butFrame, text='Очистить', width=12,
+               command=lambda event=None, draw_canv=self.c, flag=True: redraw(event, draw_canv,
+                                                                         setarrow=flag)) \
+            .pack(side=LEFT, padx=(10, 5), pady=(5, 10))
+        Button(butFrame, text='Закрыть', width=12, command=lambda flag=0: sys.exit(flag)).pack(side=LEFT, padx=(5, 10),
+                                                                                               pady=(5, 10))
+        columns = ('#1', '#2')
+        params = Treeview(setup2, show='headings', columns=columns, height=5)
+        params.heading('#1', text='Параметр')
+        params.heading('#2', text='Значение')
+        params.column('#1', width=100, minwidth=50, stretch=NO, anchor=N)
+        params.column('#2', width=100, minwidth=50, stretch=NO, anchor=N)
+        params.pack(side=TOP, padx=(5, 10), pady=(0, 10))
+
+        butFrame.pack(side=TOP)
+
+        setup1.pack(side=LEFT)
+        setup2.pack(side=LEFT)
+
+        canv.pack(side=TOP, pady=(10, 10))
+        setup.pack(side=TOP, pady=(0, 10), padx=(25, 25))
+
+        window.bind('<Button-1>', press)
+        window.bind('<ButtonRelease-1>', lambda event, draw_canv=self.c: release(event, draw_canv))
+        self.draw_mirror()
+        window.mainloop()
+
+    def redraw(self, event):
+        self.c.delete('all')
+        self.draw_mirror()
+
+    def draw_mirror(self):
+        c1, self.mirror_border = self.math.mirror(self.scal.get(), self.angle.get())
+        self.c.create_line(list(c1), width=3, fill='red')
+
+        # отображение центра зеркала
+        self.c.create_oval(self.x_center, self.y_center, self.x_center - 5, self.y_center-5, fill='green')
+
+
+
+def main():
+    app = App(350)
 
 
 if __name__ == '__main__':
